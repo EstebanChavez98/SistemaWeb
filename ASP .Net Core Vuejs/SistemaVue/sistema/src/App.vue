7880224d7e99586c58762d4ed980d455be301cc0
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-12-19 13:45:28 -0700

    Create/Update Data and update status from categoria

--- ASP .Net Core Vuejs/SistemaVue/sistema/src/App.vue
+++ ASP .Net Core Vuejs/SistemaVue/sistema/src/App.vue
@@ -204,7 +204,7 @@
         <v-flex text-xs-center>
           <v-card flat tile color="primary" class="white--text">
             <v-card-text class="white--text pt-0">
-              IncanatoIT &copy;2018
+              Esteban Chavez &copy;2019
             </v-card-text>
           </v-card>
         </v-flex>
--- ASP .Net Core Vuejs/SistemaVue/sistema/src/main.js
+++ ASP .Net Core Vuejs/SistemaVue/sistema/src/main.js
@@ -3,9 +3,10 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://localhost:55425/'
 new Vue({
   router,
   store,
