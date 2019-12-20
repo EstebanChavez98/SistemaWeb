8eb5c5a879d82b1da0a722659fe84fe6289f0100
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-12-20 13:52:23 -0700

    Creation of Rol Controller

--- ASP .Net Core Vuejs/Sistema/Sistema.Datos/Mapping/Usuarios/RolMap.cs
+++ ASP .Net Core Vuejs/Sistema/Sistema.Datos/Mapping/Usuarios/RolMap.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema.Entidades.Usuarios;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sistema.Datos.Mapping.Usuarios
 {
-    class RolMap
+    public class RolMap : IEntityTypeConfiguration<Rol>
     {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Rol> builder)
+        {
+            builder.ToTable("rol")
+                .HasKey(r => r.idrol);
+        }
     }
 }
