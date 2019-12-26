892377a88dca624c2c9c03a29de2ed545ab9701c
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-12-26 14:37:21 -0700

    Mapping of Persona Entity

--- ASP .Net Core Vuejs/BackEnd ASP .Net/Sistema.Datos/DbContextSistema.cs
+++ ASP .Net Core Vuejs/BackEnd ASP .Net/Sistema.Datos/DbContextSistema.cs
@@ -4,8 +4,10 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Sistema.Datos.Mapping.Almacen;
 using Sistema.Datos.Mapping.Usuarios;
+using Sistema.Datos.Mapping.Ventas;
 using Sistema.Entidades.Almacen;
 using Sistema.Entidades.Usuarios;
+using Sistema.Entidades.Ventas;
 
 namespace Sistema.Datos
 {
@@ -15,6 +17,7 @@ namespace Sistema.Datos
         public DbSet<Articulo> Articulos { get; set; }
         public DbSet<Rol> Roles { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Persona> Personas { get; set; }
         //Constructor
         public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
         {
@@ -29,6 +32,7 @@ namespace Sistema.Datos
             modelBuilder.ApplyConfiguration(new ArticuloMap());
             modelBuilder.ApplyConfiguration(new RolMap());
             modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new PersonaMap());
         }
     }
 }
